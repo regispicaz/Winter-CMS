d521619ee1410da2570d26ae1045a942f29232c8
Régis Picáz <registavares@protonmail.com>
2023-10-30 00:11:48 -0300

    Feat: Adição de sortable em páginas de produtos(backend)

--- plugins/regis/conent/models/Produto.php
+++ plugins/regis/conent/models/Produto.php
@@ -8,7 +8,8 @@ use Model;
 class Produto extends Model
 {
     use \October\Rain\Database\Traits\Validation;
-    
+    use \October\Rain\Database\Traits\Sortable;
+
 
     /**
      * @var string The database table used by the model.
